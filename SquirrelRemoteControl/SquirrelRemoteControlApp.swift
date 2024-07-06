2887df7c95d04c9465baf3ad912235669b2343e6
ornwoo96 <73861795+ornwoo96@users.noreply.github.com>
2024-07-06 19:37:19 +0900

    Initial Commit

---
+++ SquirrelRemoteControl/SquirrelRemoteControlApp.swift
@@ -0,0 +1,32 @@
+//
+//  SquirrelRemoteControlApp.swift
+//  SquirrelRemoteControl
+//
+//  Created by DongWoo Kim on 7/6/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SquirrelRemoteControlApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
